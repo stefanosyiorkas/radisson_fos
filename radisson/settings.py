0e9061b6c520e44fd7712fc98f85953aeed35e56
Stefanos Giorkas <stefanos.y@hotmail.com>
2022-11-30 13:31:14 +0200

    debug true settings

--- radisson/settings.py
+++ radisson/settings.py
@@ -22,7 +22,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'i0&iq&e9u9h6(4_7%pt2s9)f=c$kso=k$c$w@fi9215s=1q0^d'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+# WHEN CHANGING RUN python manage.py collectstatic
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1','sgiorkas.pythonanywhere.com','sql7.freemysqlhosting.net']
 CSRF_TRUSTED_ORIGINS = ['https://sgiorkas.pythonanywhere.com']
@@ -61,13 +62,13 @@ TINYMCE_DEFAULT_CONFIG = {
 # Application definition
 
 INSTALLED_APPS = [
-    'whitenoise.runserver_nostatic',
     'orders.apps.OrdersConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    'whitenoise.runserver_nostatic',
     'django.contrib.staticfiles',
     'django.contrib.humanize',
     'tinymce',
@@ -176,12 +177,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
-if DEBUG:
-    STATICFILES_DIRS = [
-            os.path.join(BASE_DIR, 'orders/static'),
-       ]
-else:
-    STATIC_ROOT = os.path.join(BASE_DIR, 'orders/static')
+# if DEBUG:
+#     STATICFILES_DIRS = [
+#             os.path.join(BASE_DIR, 'orders/static'),
+#        ]
+# else:
+#     STATIC_ROOT = os.path.join(BASE_DIR, 'orders/static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'orders/static')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
